a3c2063ee3de0292f6d10d8b21c9c0138556188b
yelmiller <yelyahmiller@icloud.com>
2025-05-12 08:47:27 -0400

    Update main.tf

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,12 +1,7 @@
-
 resource "aws_instance" "web" {
-  ami           = ami-0c55b159cbfafe1f0
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = var.instance_type
-  tags = {
-    Name = "ACME-Web"
-  }
+  tags = { Name = "ACME-Web" }
 }
 
-output "public_ip" {
-  value = aws_instance.web.public_ip
-}
+output "public_ip" { value = aws_instance.web.public_ip }
