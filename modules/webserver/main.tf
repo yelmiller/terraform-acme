eecf2d112440f1620f9bff4df0a609b9b4f1354b
yelmiller <yelyahmiller@icloud.com>
2025-05-12 09:35:08 -0400

    Update main.tf

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -4,7 +4,6 @@ data "aws_ami" "amazon_linux" {
   filter { name="name" values=["amzn2-ami-hvm-*-x86_64-gp2"] }
 }
 
-
 resource "aws_instance" "web" {
   ami = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
