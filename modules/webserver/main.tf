d5111d8d5d7de02bbe93ed497272ec6ae0c27ecd
yelmiller <yelyahmiller@icloud.com>
2025-05-11 23:56:55 -0400

    Remove duplicate instance_type variable from main.tf

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,9 +1,3 @@
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = var.instance_type
