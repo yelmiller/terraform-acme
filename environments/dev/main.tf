b0e08284e4266d3a0a3cca55294061392b43892e
yelmiller <yelyahmiller@icloud.com>
2025-05-12 08:29:58 -0400

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,7 @@
 variable "instance_type" {
   description = "Instance type passed to module"
   type        = string
-  default     = "t2.small"
+  default     = "t3.micro"
 }
 
 module "webserver" {
