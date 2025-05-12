420347e39f2b5294bf74b0d4258125434452dc77
yelmiller <yelyahmiller@icloud.com>
2025-05-12 03:08:29 -0400

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = "us-east-1"
 }
