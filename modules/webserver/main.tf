7af664e4aa75b794c66921ab0b04f4341bef3bd3
yelmiller <yelyahmiller@icloud.com>
2025-05-12 03:24:47 -0400

    Update main.tf

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,7 +1,11 @@
-data "aws_ami" "amazon_linux" { most_recent = true owners = ["amazon"] 
-
-filter { name = "name" values = ["amzn2-ami-hvm-*-x86_64-gp2"] } } 
-
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
 resource "aws_instance" "web" {
   ami           = "ami = data.aws_ami.amazon_linux.id"
   instance_type = var.instance_type
