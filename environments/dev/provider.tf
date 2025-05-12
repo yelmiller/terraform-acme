25bc4e97d1fa61e002cea7b45aa200e19ee4207a
yelmiller <yelyahmiller@icloud.com>
2025-05-12 03:05:07 -0400

    Add provider.tf with explicit AWS cred variables

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,3 +1,8 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
 provider "aws" {
-  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
 }
