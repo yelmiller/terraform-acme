736792ca0edb76875851ec82d6b67569630074b1
yelmiller <yelyahmiller@icloud.com>
2025-05-12 01:07:38 -0400

    Use aws_ami data source instead of hard-coded AMI

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -1,12 +1,12 @@
- data "aws_ami" "amazon_linux" {
-   most_recent = true
-   owners      = ["amazon"]
-
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
   filter {
-     name   = "name"
-     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-   }
- }
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
